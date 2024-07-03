0c773a638659be9e318a16eb12fa57ae3a49b477
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-07-03 18:13:04 -0400

    messed with framework

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+
+// $(document).ready(function () {
+//     $('#searchButton').click(function () {
+//         var searchTerm = $('#search-input').val().toLowerCase();
+//         // Perform search logic here
+//         // You can filter content based on the search term
+//     });
+// });
